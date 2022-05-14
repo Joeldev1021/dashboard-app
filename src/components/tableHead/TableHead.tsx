592909ce26a5d289b0b5feaf469057757b58a6a5
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-14 10:34:27 -0500

    fix: handle check task into the component

--- src/components/tableHead/TableHead.tsx
+++ src/components/tableHead/TableHead.tsx
@@ -1,21 +1,29 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Task } from "../../interface/TaskInterface";
 
 interface props {
-    isCheck: Task[];
+    tasks: Task[];
     handleShowFilter: () => void;
-    handleShowModal: () => void;
+    setShowModal: (value: boolean) => void;
 }
 
-const TableHead = ({ isCheck, handleShowFilter, handleShowModal }:props) => {
+const TableHead = ({ tasks, handleShowFilter, setShowModal }:props) => {
+  const [tasksIsCheck, setTasksIsCheck] = useState(tasks.filter((task:Task) => task.select === true));
+
+  /* update tasksIscheck */
+  useEffect(() => {
+    setTasksIsCheck(tasks.filter((task:Task) => task.select === true));
+  }, [tasks]);
+
   return (
     <div className="table__head">
       <button onClick={() => handleShowFilter()} className="btn__filter">
         <i className="fas fa-filter"></i>
       </button>
-      {isCheck.length > 0 && (
-        <button onClick={() => handleShowModal()} className="btn btn__delete">
-          Delete {`(${isCheck.length})`}
+      {tasksIsCheck.length > 0 && (
+        <button onClick={() => setShowModal(true)} className="btn btn__delete">
+          Delete {`(${tasksIsCheck.length})`}
         </button>
       )}
 
