ab74a624e87be4f302fd6a29159b3f5bf8619edf
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-14 15:15:04 -0500

    refactor ListItem

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -7,39 +7,32 @@ import { Task } from "../../interface/TaskInterface";
 import Progress from "../Progress/Progress";
 
 interface props {
-  item: Task;
-  handleIsCheck: (task: Task) => void;
+  task: Task;
 }
 
-const ListItem = ({ item, handleIsCheck }: props) => {
+const ListItem = ({ task }: props) => {
   const { addSelectTask } = useContext(TaskContext);
-
-  const handleCheck = (e: ChangeEvent) => {
-    const id = e.currentTarget.getAttribute("data-id");
-    addSelectTask(Number(id));
-  };
 
   return (
     <tr>
       <th>
-        <input type="checkbox" name="select" data-id={item.id}
-        onChange={(e) => handleCheck(e)}
-        onClick={(e) => handleIsCheck(item)}
-        checked={item.select}
+        <input type="checkbox" name="select"
+        onChange={(e) => addSelectTask(task.id!)}
+        checked={task.select}
          />
       </th>
-      <td>{item.title}</td>
-      <td>{item.user}</td>
+      <td>{task.title}</td>
+      <td>{task.user}</td>
       <td style={{ textAlign: "center" }}>
-        <button className={`btn btn_status ${item.status}`}>
-          {item.status}
+        <button className={`btn btn_status ${task.status}`}>
+          {task.status}
         </button>
       </td>
       <td style={{ textAlign: "center" }}>
-        <i className={`fas fa-flag ${item.priority}`}></i>
+        <i className={`fas fa-flag ${task.priority}`}></i>
       </td>
-      <td><Progress task={item}/></td>
-      <td>{item.endDate}</td>
+      <td><Progress task={task}/></td>
+      <td>{task.endDate}</td>
     </tr>
   );
 };
