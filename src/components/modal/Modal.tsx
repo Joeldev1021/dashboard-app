1ba957d640db16bac1e79b7d20dc363b7baafd5a
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-14 16:23:01 -0500

    feat: remove task into the modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -11,6 +11,7 @@ interface Props {
 
 const Modal = ({ setShowModal, tasks }: Props) => {
   const { removeTasks } = useContext(TaskContext);
+
   const handleRemove = () => {
     removeTasks();
     setShowModal(false);
